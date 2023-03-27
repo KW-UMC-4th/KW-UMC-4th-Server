bbb8760bee318eca7e4e723c3b7352b12b226a2e
Ji Seungmin <cswcsm02@gmail.com>
2023-03-27 14:56:46 +0900

    init

---
+++ src/main/java/kw/seminar/SeminarApplication.java
@@ -0,0 +1,13 @@
+package kw.seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kw/seminar/SeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
